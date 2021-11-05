0c8a8df64d07e84ca58affa64e96eeb2eecf7a8a
BrahianVT <pumasemj@hotmail.com>
2021-11-05 12:03:48 -0600

    test with hash router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import {BrowserRouter, Switch, Route } from "react-router-dom";
+import {HashRouter, Switch, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import pj from '../package.json';
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route path={"/" + pj.name}  component={Main}/>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
