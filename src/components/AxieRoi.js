6476bf11afba72d4651f7739c5271774ab3ebaa1
BrahianVT <pumasemj@hotmail.com>
2022-02-14 11:21:34 -0600

    updated axie roi

--- src/components/AxieRoi.js
+++ src/components/AxieRoi.js
@@ -43,7 +43,7 @@ export default function AxieRoi() {
     const [slpPrice, setslpPrice] = React.useState(0)
     const [priceTeam, setPriceTeam] = React.useState(750)
     const [rows, setRows] = React.useState([])
-    
+    const[ rows2, setRows2] = React.useState([])
     React.useEffect(() => {
         gtSLPPrice(setslpPrice)    
     },[])
@@ -58,6 +58,15 @@ export default function AxieRoi() {
     var seven = ['2000', 18, getEarnings(quest, mission, 1, 18), getEarnings(quest, mission, 7, 18), getEarnings(quest, mission, 30, 18), slpPrice * getEarnings(quest, mission, 30, 18), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 18))]
     var eigth = ['2200', 21, getEarnings(quest, mission, 1, 21), getEarnings(quest, mission, 7, 21), getEarnings(quest, mission, 30, 21), slpPrice * getEarnings(quest, mission, 30, 21), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 21))]
 
+    var one2 = []
+    var two2 = []
+    var three2 = []
+    var four2 = []
+    var five2 = []
+    var six2 = []
+    var seven2 = []
+    var eigth2 = []
+
     React.useEffect( () => {
         one = ['800', 1, getEarnings(quest, mission, 1, 1), getEarnings(quest, mission, 7, 1), getEarnings(quest, mission, 30, 1), slpPrice * getEarnings(quest, mission, 30, 1), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 1))]
         two = ['1000', 3, getEarnings(quest, mission, 1, 3), getEarnings(quest, mission, 7, 3), getEarnings(quest, mission, 30, 3), slpPrice * getEarnings(quest, mission, 30, 3), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 3))]
@@ -68,6 +77,16 @@ export default function AxieRoi() {
         seven = ['2000', 18, getEarnings(quest, mission, 1, 18), getEarnings(quest, mission, 7, 18), getEarnings(quest, mission, 30, 18), slpPrice * getEarnings(quest, mission, 30, 18), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 18))]
         eigth = ['2200', 21, getEarnings(quest, mission, 1, 21), getEarnings(quest, mission, 7, 21), getEarnings(quest, mission, 30, 21), slpPrice * getEarnings(quest, mission, 30, 21), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 21))]
         setRows([one, two, three, four, five, six, seven, eigth])
+        quest = 0; mission = 0
+        one2 = ['999', 1, getEarnings(quest, mission, 1, 1), getEarnings(quest, mission, 7, 1), getEarnings(quest, mission, 30, 1), slpPrice * getEarnings(quest, mission, 30, 1), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 1))]
+        two2 = ['1000', 3, getEarnings(quest, mission, 1, 3), getEarnings(quest, mission, 7, 3), getEarnings(quest, mission, 30, 3), slpPrice * getEarnings(quest, mission, 30, 3), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 3))]
+        three2 = ['1100', 5, getEarnings(quest, mission, 1, 5), getEarnings(quest, mission, 7, 5), getEarnings(quest, mission, 30, 5), slpPrice * getEarnings(quest, mission, 30, 5), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 5))]
+        four2 = ['1300', 6, getEarnings(quest, mission, 1, 6), getEarnings(quest, mission, 7, 6), getEarnings(quest, mission, 30, 6), slpPrice * getEarnings(quest, mission, 30, 6), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 6))]
+        five2 = ['1500', 8, getEarnings(quest, mission, 1, 8), getEarnings(quest, mission, 7, 8), getEarnings(quest, mission, 30, 8), slpPrice * getEarnings(quest, mission,30, 8), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 8))]
+        six2 = ['1800', 10, getEarnings(quest, mission, 1, 10), getEarnings(quest, mission, 7, 10), getEarnings(quest, mission, 30, 10), slpPrice * getEarnings(quest, mission, 30, 10), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 10))]
+        seven2 = ['2000', 12, getEarnings(quest, mission, 1, 12), getEarnings(quest, mission, 7, 12), getEarnings(quest, mission, 30, 12), slpPrice * getEarnings(quest, mission, 30, 12), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 12))]
+        eigth2 = ['2500', 14, getEarnings(quest, mission, 1, 14), getEarnings(quest, mission, 7, 14), getEarnings(quest, mission, 30, 14), slpPrice * getEarnings(quest, mission, 30, 14), priceTeam / (slpPrice * getEarnings(quest, mission, 30, 14))]
+        setRows2([one2, two2, three2, four2, five2, six2, seven2, eigth2])
 
     },[priceTeam, slpPrice])
     
@@ -88,6 +107,8 @@ export default function AxieRoi() {
                     InputProps={{ startAdornment: <InputAdornment position="start">$</InputAdornment> }}
                 />
             </Stack>
+
+            <ListItemText primary={"BEFORE UPDATE"}  />
             <Stack  sx ={{bgcolor: '#FFA533' , width: 450}} direction="row">
                 <ListItemText primary={'Quest : 25'} />
                 <ListItemText primary={'Mission : 50'} />
@@ -122,6 +143,36 @@ export default function AxieRoi() {
                     </TableBody>
                 </Table>
             </TableContainer>
+
+            <ListItemText primary={"AFTER UPDATE"}  />
+            <TableContainer component={Paper}>
+                <Table sx={{ ...styles }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow >
+                            <StyledTableCell >MMR</StyledTableCell>
+                            <StyledTableCell >Slp/Win</StyledTableCell>
+                            <StyledTableCell >Slp/Day</StyledTableCell>
+                            <StyledTableCell >Slp/Week</StyledTableCell>
+                            <StyledTableCell >Slp/Month</StyledTableCell>
+                            <StyledTableCell >Earns</StyledTableCell>
+                            <StyledTableCell >ROI</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows2.map((row) => (
+                            <TableRow  key={row[0]}>
+                            <TableCell sx={{bgcolor: '#1CD58E' }}>{row[0]} </TableCell>
+                            <TableCell sx={{bgcolor: '#FFC2EF' }}>{row[1]} </TableCell>
+                            <TableCell sx={{bgcolor: '#FFC2EF' }}>{row[2]} </TableCell>
+                            <TableCell sx={{bgcolor: '#FFC2EF' }}>{row[3]} </TableCell>
+                            <TableCell sx={{bgcolor: '#FFC2EF' }}>{row[4]} </TableCell>
+                            <TableCell sx={{bgcolor: '#FF5733' }}>{'$ '  + row[5].toFixed(2)} </TableCell>
+                            <TableCell sx={{bgcolor: '#FF5733' }}>{row[6].toFixed(2)} </TableCell>
+                            </TableRow >
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>               
         </Box>
 
     );
